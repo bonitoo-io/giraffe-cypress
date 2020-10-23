eb2601d7f03fe6eba456f7d64f374d0d2b838e47
karel-rehor <karel.k.rehor@email.cz>
2020-10-23 16:42:58 +0200

    fix: check for already installed dependencies in jenkins script

--- scripts/jenkins-headless-conf.sh
+++ scripts/jenkins-headless-conf.sh
@@ -2,15 +2,32 @@
 
 # to be used to configure jenkins nodes
 
-#TODO add checks so as to not reinstall
+echo "=============== Installing NODE 12 ===="
+NODE_VER=$(node --version)
+exit_status=$?
 
-# install preferred npm
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install -y nodejs
+if [ $exit_status -eq 0 ]; then
+    echo "NODEJS already installed ${NODE_VER}"
+else
+    echo "Installing node and npm"
+    # install preferred npm
+    curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+fi
 
-# install yarn (needed for workspaces)
-npm install yarn -g
+echo "=============== Installing Yarn ===="
+YARN_VER=$(yarn --version)
+exit_status=$?
 
+if [ $exit_status -eq 0 ]; then
+    echo "Yarn already installed ${YARN_VER}"
+else
+    echo "Installing yarn globally"
+    # install yarn (needed for workspaces)
+    sudo npm install yarn -g
+fi
+
+echo "=============== Installing XVFB ===="
 # setup xvfb for cypress
 sudo apt-get -y install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev \
 libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
