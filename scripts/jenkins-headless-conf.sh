58935bb84bba599f57aa052b6f849e45514e424b
karel-rehor <karel.k.rehor@email.cz>
2020-10-23 16:27:13 +0200

    fix: activate jenkins conf script

--- scripts/jenkins-headless-conf.sh
+++ scripts/jenkins-headless-conf.sh
@@ -2,6 +2,8 @@
 
 # to be used to configure jenkins nodes
 
+#TODO add checks so as to not reinstall
+
 # install preferred npm
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
@@ -12,7 +14,3 @@ npm install yarn -g
 # setup xvfb for cypress
 sudo apt-get -y install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev \
 libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
-
-
-
-
