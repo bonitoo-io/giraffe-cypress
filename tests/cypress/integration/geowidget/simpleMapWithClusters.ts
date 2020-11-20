82ccf121ea55426157338b5c4f44e2a03d610122
karel-rehor <karel.k.rehor@email.cz>
2020-11-20 17:10:52 +0100

    chore: additional tests for clusters - WIP

--- tests/cypress/integration/geowidget/simpleMapWithClusters.ts
+++ tests/cypress/integration/geowidget/simpleMapWithClusters.ts
@@ -29,9 +29,46 @@ describe('GeoWidget - Basic - Marker Clusters', () => {
 
     it('shows cluster polygons', () => {
         cy.get(' g > path').should('not.be.visible')
-        cy.get('.marker-cluster-custom').eq(3).trigger('mouseover')
-        cy.get(' g > path').should('be.visible')
-        cy.get('.marker-cluster-custom').eq(3).trigger('mouseout')
-        cy.get(' g > path').should('not.be.visible')
+        for(let i = 0; i < 8; i++){
+            cy.get('.marker-cluster-custom').eq(i).trigger('mouseover')
+            cy.get(' g > path').should('be.visible')
+            cy.get('.marker-cluster-custom').eq(i).trigger('mouseout')
+            cy.get(' g > path').should('not.be.visible')
+        }
+    })
+
+    it('shows cluster marker count at level 8', () => {
+        let expectedCounts = [9,10,5,13,3,10,4,5]
+        for(let i = 0; i < 8; i++){
+            cy.get('div.marker-cluster-custom > div > div').eq(i).contains(expectedCounts[i]);
+        }
+    })
+
+    it('shows cluster group colors at level 8', () => {
+        let expectedColors = ['rgb(255, 79, 0)',
+            'rgb(255, 211, 0)',
+            'rgb(255, 79, 0)',
+            'rgb(255, 79, 0)',
+            'rgb(255, 211, 0)',
+            'rgb(255, 211, 0)',
+            'rgb(255, 79, 0)',
+            'rgb(255, 79, 0)']
+        for(let i = 0; i < 8; i++){
+            cy.get('.marker-cluster-custom > div').eq(i).should('have.css','background-color', expectedColors[i])
+        }
+
+    })
+
+    //See Giraffe issue 387
+    it.skip('shows tooltips for unclustered markers', () => {
+       // cy.get('.svg-icon').eq(0).trigger('mouseover');
+    })
+
+    it.skip('breaks down clusters on zoom in', () => {
+
+    })
+
+    it.skip('aggregates clusters on zoom out', () => {
+
     })
 })
