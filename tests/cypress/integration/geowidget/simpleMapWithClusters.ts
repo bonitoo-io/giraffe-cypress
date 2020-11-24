9dd55a7d8eba0c0a0d02912969a45d9e7faac4bc
karel-rehor <karel.k.rehor@email.cz>
2020-11-24 17:31:13 +0100

    chore: add zoom tests on clusters - WIP

--- tests/cypress/integration/geowidget/simpleMapWithClusters.ts
+++ tests/cypress/integration/geowidget/simpleMapWithClusters.ts
@@ -62,9 +62,8 @@ describe('GeoWidget - Basic - Marker Clusters', () => {
        // cy.get('.svg-icon').eq(0).trigger('mouseover');
     })
 
-    it.only('breaks down clusters on basic zoom in', () => {
-        //do zoom in once
-        let expectedCounts = [[2,2,2,8,3,2,2,2]]
+    it('breaks down clusters on basic zoom in', () => {
+        let expectedCounts = [[2,2,2,8,3,2,2,2],[2,5]]
         let expectedColors = [['rgb(255, 79, 0)',
             'rgb(255, 211, 0)',
             'rgb(255, 211, 0)',
@@ -72,8 +71,10 @@ describe('GeoWidget - Basic - Marker Clusters', () => {
             'rgb(255, 79, 0)',
             'rgb(255, 211, 0)',
             'rgb(255, 79, 0)',
-            'rgb(219, 0, 0)'
-        ]]
+            'rgb(219, 0, 0)'],
+            ['rgb(255, 211, 0)','rgb(255, 79, 0)']
+        ]
+        //do zoom in once
         cy.get('[title=\'Zoom in\'][role=button]')
             .click()
             .wait(1000) //wait for zoom to load
@@ -110,17 +111,269 @@ describe('GeoWidget - Basic - Marker Clusters', () => {
             expect(visibleCt).to.equal(2);
         })
         // Zoom Again
-     /*   cy.get('[title=\'Zoom in\'][role=button]')
+        cy.get('[title=\'Zoom in\'][role=button]')
             .click()
             .wait(1000) //wait for zoom to load
+
+        // check clustered markers
         cy.get('.marker-cluster-custom').then(markers => {
             let visibleCt = 0;
             cy.log('DEBUG markers.length ' + markers.length)
-        }) */
+            for(let i = 0; i < 12; i++){
+                //cy.log(`${i} is visible: ${markers.eq(i).is(':visible')}`)
+                if(markers.eq(i).is(':visible')){
+                    cy.get('.marker-cluster-custom > div')
+                        .eq(i)
+                        .should('have.css','background-color', expectedColors[1][visibleCt])
+                    expect(parseInt(markers.eq(i).text()))
+                        .to.equal(expectedCounts[1][visibleCt++]);
+                }
+            }
+            expect(visibleCt).to.equal(2)
+        })
+
+        //check individual markers
+        cy.get('.leaflet-marker-icon > svg').then(markers => {
+            cy.log('DEBUG svg markers.length ' + markers.length);
+            let visibleCt = 0;
+            let expectedMarkerColors = ['#db0000']
+            for(let i = 0; i < 16; i++){
+                if(markers.eq(i).is(':visible')){
+                    expect(markers.eq(i).find('path').attr('fill'))
+                        .to
+                        .equal(expectedMarkerColors[visibleCt++]);
+                }
+            }
+            expect(visibleCt).to.equal(1);
+        })
+
 
     })
 
-    it.skip('aggregates clusters on zoom out', () => {
+    it('aggregates clusters on basic zoom out', () => {
+        let expectedCounts = [[9,10,5,13,3,10,4,5],[9,29,13,9],[60]]
+        let expectedColors = [['rgb(255, 79, 0)',
+            'rgb(255, 211, 0)',
+            'rgb(255, 79, 0)',
+            'rgb(255, 79, 0)',
+            'rgb(255, 211, 0)',
+            'rgb(255, 211, 0)',
+            'rgb(255, 79, 0)',
+            'rgb(255, 79, 0)'
+            ],
+            ['rgb(255, 79, 0)',
+                'rgb(255, 79, 0)',
+                'rgb(255, 211, 0)',
+                'rgb(255, 79, 0)',
+            ],
+            ['rgb(255, 79, 0)']
+        ]
+
+        //check baseline
+        cy.get('.marker-cluster-custom').then(markers => {
+            let visibleCt = 0;
+            for(let i = 0; i < 8; i++){
+                //cy.log(`${i} is visible: ${markers.eq(i).is(':visible')}`)
+                if(markers.eq(i).is(':visible')){
+                    cy.get('.marker-cluster-custom > div')
+                        .eq(i)
+                        .should('have.css','background-color', expectedColors[0][visibleCt])
+                    expect(parseInt(markers.eq(i).text()))
+                        .to.equal(expectedCounts[0][visibleCt++]);
+                }
+            }
+            expect(visibleCt).to.equal(8)
+        })
+
+        cy.get('.leaflet-marker-icon > svg').then(markers => {
+            cy.log('DEBUG svg markers.length ' + markers.length);
+            let visibleCt = 0;
+            let expectedMarkerColors = ['#FFD300']
+            for(let i = 0; i < 8; i++){
+                if(markers.eq(i).is(':visible')){
+                    expect(markers.eq(i).find('path').attr('fill'))
+                        .to
+                        .equal(expectedMarkerColors[visibleCt++]);
+                }
+            }
+            expect(visibleCt).to.equal(1);
+        })
+        //zoom out once
+        cy.get('[title=\'Zoom out\'][role=button]')
+            .click()
+            .wait(1000) //wait for zoom to load
+
+        //Check clustered markers
+        cy.get('.marker-cluster-custom').then(markers => {
+            let visibleCt = 0;
+            for(let i = 0; i < 4; i++){
+                //cy.log(`${i} is visible: ${markers.eq(i).is(':visible')}`)
+                if(markers.eq(i).is(':visible')){
+                    cy.get('.marker-cluster-custom > div')
+                        .eq(i)
+                        .should('have.css','background-color', expectedColors[1][visibleCt])
+                    expect(parseInt(markers.eq(i).text()))
+                        .to.equal(expectedCounts[1][visibleCt++]);
+                }
+            }
+            expect(visibleCt).to.equal(4)
+        })
+
+        cy.get('.leaflet-marker-icon > svg').should('have.lengthOf', 0)
+
+        //zoom out again
+        cy.get('[title=\'Zoom out\'][role=button]')
+            .click()
+            .wait(1000) //wait for zoom to load
+
+        //Check clustered markers
+        cy.get('.marker-cluster-custom').then(markers => {
+            let visibleCt = 0;
+            for(let i = 0; i < 1; i++){
+                //cy.log(`${i} is visible: ${markers.eq(i).is(':visible')}`)
+                if(markers.eq(i).is(':visible')){
+                    cy.get('.marker-cluster-custom > div')
+                        .eq(i)
+                        .should('have.css','background-color', expectedColors[2][visibleCt])
+                    expect(parseInt(markers.eq(i).text()))
+                        .to.equal(expectedCounts[2][visibleCt++]);
+                }
+            }
+            expect(visibleCt).to.equal(1)
+        })
+
+        cy.get('.leaflet-marker-icon > svg').should('have.lengthOf', 0)
+
+    })
+
+    it('expands and zooms clustered markers on click', () => {
+
+        let expectedCounts = [[2,8,3,2],[2,5],[4]]
+        let expectedColors = [['rgb(255, 211, 0)',
+            'rgb(255, 79, 0)',
+            'rgb(255, 79, 0)',
+            'rgb(255, 79, 0)'],
+            ['rgb(255, 211, 0)',
+             'rgb(255, 79, 0)'],
+            ['rgb(255, 79, 0)']
+        ]
+
+        //TODO - Better wait
+        //zoom in on largest cluster
+        cy.get('.marker-cluster-custom').eq(3).click().wait(3000) // level 9 - visible 4 clustered 2 markers
+        //check level
+        cy.get('.leaflet-tile-container > img').eq(0).invoke('attr','src').then(url => {
+            cy.parseLeafletTileSrc(url as string).should(src => {
+                expect(src.layer).to.equal(9);
+            })
+        })
+
+        //TODO - refactor following into generalized method
+        // Check cluster markers
+        cy.get('.marker-cluster-custom').then(markers => {
+            let visibleCt = 0;
+            for(let i = 0; i < 17; i++){
+                //cy.log(`${i} is visible: ${markers.eq(i).is(':visible')}`)
+                if(markers.eq(i).is(':visible')){
+                    cy.get('.marker-cluster-custom > div')
+                        .eq(i)
+                        .should('have.css','background-color', expectedColors[0][visibleCt])
+                    expect(parseInt(markers.eq(i).text()))
+                        .to.equal(expectedCounts[0][visibleCt++]);
+                }
+            }
+            expect(visibleCt).to.equal(4)
+        })
+
+        //TODO - refactor following into generalized method
+        //check individual markers
+        cy.get('.leaflet-marker-icon > svg').then(markers => {
+            cy.log('DEBUG svg markers.length ' + markers.length);
+            let visibleCt = 0;
+            let expectedMarkerColors = ['#db0000',
+                '#db0000']
+            for(let i = 0; i < 8; i++){
+                if(markers.eq(i).is(':visible')){
+                    expect(markers.eq(i).find('path').attr('fill'))
+                        .to
+                        .equal(expectedMarkerColors[visibleCt++]);
+                }
+            }
+            expect(visibleCt).to.equal(2);
+        })
+
+        //TODO - better wait
+        //zoom in on largest cluster of result
+        cy.get('.marker-cluster-custom').eq(7).click().wait(3000) // level 11 - visible 1 cluster 4 markers
+        cy.get('.leaflet-tile-container > img').eq(0).invoke('attr','src').then(url => {
+            cy.parseLeafletTileSrc(url as string).should(src => {
+                expect(src.layer).to.equal(11);
+            })
+        })
+
+        // Check cluster markers
+        cy.get('.marker-cluster-custom').then(markers => {
+            let visibleCt = 0;
+            for(let i = 0; i < 2; i++){
+                //cy.log(`${i} is visible: ${markers.eq(i).is(':visible')}`)
+                if(markers.eq(i).is(':visible')){
+                    cy.get('.marker-cluster-custom > div')
+                        .eq(i)
+                        .should('have.css','background-color', expectedColors[2][visibleCt])
+                    expect(parseInt(markers.eq(i).text()))
+                        .to.equal(expectedCounts[2][visibleCt++]);
+                }
+            }
+            expect(visibleCt).to.equal(1)
+        })
+
+        //check individual markers
+        cy.get('.leaflet-marker-icon > svg').then(markers => {
+            cy.log('DEBUG svg markers.length ' + markers.length);
+            let visibleCt = 0;
+            let expectedMarkerColors = ['#db0000',
+                '#FF4F00',
+                '#006d6f',
+                '#006d6f']
+            for(let i = 0; i < 6; i++){
+                if(markers.eq(i).is(':visible')){
+                    expect(markers.eq(i).find('path').attr('fill'))
+                        .to
+                        .equal(expectedMarkerColors[visibleCt++]);
+                }
+            }
+            expect(visibleCt).to.equal(4);
+        })
+
+        //TODO better wait
+          //zoom in on only cluster remaining
+        cy.get('.marker-cluster-custom').eq(0).click().wait(3000) // level 13 - visible 0 cluster 4 markers
+        cy.get('.leaflet-tile-container > img').eq(0).invoke('attr','src').then(url => {
+            cy.parseLeafletTileSrc(url as string).should(src => {
+                expect(src.layer).to.equal(13);
+            })
+        })
+
+        cy.get('.marker-cluster-custom').should('have.lengthOf', 0)
+
+        //check individual markers
+        cy.get('.leaflet-marker-icon > svg').then(markers => {
+            cy.log('DEBUG svg markers.length ' + markers.length);
+            let visibleCt = 0;
+            let expectedMarkerColors = ['#FF4F00',
+                '#FF4F00',
+                '#FF4F00',
+                '#db0000']
+            for(let i = 0; i < 5; i++){
+                if(markers.eq(i).is(':visible')){
+                    expect(markers.eq(i).find('path').attr('fill'))
+                        .to
+                        .equal(expectedMarkerColors[visibleCt++]);
+                }
+            }
+            expect(visibleCt).to.equal(4);
+        })
+
 
     })
 })
