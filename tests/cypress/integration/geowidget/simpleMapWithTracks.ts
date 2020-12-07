3fd620840749af0a679e86da9606f1871a314f90
karel-rehor <karel.k.rehor@email.cz>
2020-12-07 17:10:20 +0100

    feat: add panning tests for tracks

--- tests/cypress/integration/geowidget/simpleMapWithTracks.ts
+++ tests/cypress/integration/geowidget/simpleMapWithTracks.ts
@@ -1,3 +1,6 @@
+
+interface dimSVG {height: number, width: number, d: string}
+
 before(() => {
     cy.resetDB();
 })
@@ -111,7 +114,7 @@ describe('GeoWidget - Basic - Map with Tracks', () => {
     //TODO adjust for firefox - has max dim height||width of svg container 730.6844496.....
     it('zooms in', {browser: '!firefox'}, () => {
 
-        let dimsBuff: {height: number, width: number, d: string}[] = [];
+        let dimsBuff: dimSVG[] = [];
 
         //Get base dims
         cy.get('svg > g > path:first-of-type').then(elems => {
@@ -193,7 +196,7 @@ describe('GeoWidget - Basic - Map with Tracks', () => {
 
     it('zoom out', () => {
 
-        let dimsBuff: {height: number, width: number, d: string}[] = [];
+        let dimsBuff: dimSVG[] = [];
 
         //Get base dims
         cy.get('svg > g > path:first-of-type').then(elems => {
@@ -251,5 +254,282 @@ describe('GeoWidget - Basic - Map with Tracks', () => {
             }
         });
     })
+
+    //see giraffe issue #403
+    it.skip("shows tooltips on tracks", () => {
+
+    })
+
+    //see giraffe issue #403
+    it.skip("shows tooltips on endpoints", () => {
+
+    })
+
+    /*
+      Tracks can disappear.
+      SVG
+          + M vals will change
+          + dims of containing rectangles will change near edges
+      + Animation speeds should remain the same
+      Endpoints:
+         + 5 Disappear
+         - Distances between endpoints remain same, but left values change
+     */
+    it("pans horizontally", () => {
+
+        let dimsBuff: dimSVG[] = [];
+        let unchangedTrackColors = ['brown', 'deeppink']
+
+        let dims: {height: number,
+            width: number,
+            left: number,
+            top: number} = {height: 0, width: 0, left: 0, top: 0}
+
+        //Get base dims
+        cy.get('svg > g > path:first-of-type').then(elems => {
+
+            for(let i = 0; i < elems.length; i++){
+                let dim = {height: 0, width: 0, d: ''}
+                dim.height = elems.eq(i).height() as number;
+                dim.width = elems.eq(i).width() as number;
+                dim.d = elems.eq(i).attr('d') as string;
+                dimsBuff.push(dim);
+            }
+        })
+
+        cy.get('div.giraffe-plot').then(container => {
+            cy.log('DEBUG typeof container.height() ' + typeof(container.height()));
+            dims.height = container.height() as number;
+            dims.width = container.width() as number;
+            let offset: JQuery.Coordinates | undefined = container.offset();
+            dims.top = offset === undefined ? 0 : offset.top;
+            dims.left = offset === undefined ? 0 : offset.left;
+        }).wait(1000)
+            .then(() => {
+                cy.log('DEBUG dims ' + JSON.stringify(dims));
+
+                // pan away
+                cy.pan('.giraffe-plot',{x:dims.width/2,y:dims.height/2},{x:dims.width - 2,y:dims.height/2})
+                    .wait(500)
+
+
+
+                //Check only 5 endpoints are visible
+                cy.get('.leaflet-container').then(container => {
+                    cy.get('svg:first-of-type > g > path').then(elems => {
+
+                        cy.calcVisibleElements(container,elems).then(count => {
+                            expect(count).to.equal(5)
+                        })
+
+                        cy.calcHiddenElements(container,elems).then(count => {
+                            expect(count).to.equal(7)
+                        })
+                    })
+                })
+
+                //Check dim vals changed for some tracks
+                cy.get('svg > g > path:first-of-type').then(elems => {
+
+                    for(let i = 1; i < elems.length; i++){
+                        let dim = {height: 0, width: 0, d: ''}
+                        dim.height = elems.eq(i).height() as number;
+                        dim.width = elems.eq(i).width() as number;
+                        dim.d = elems.eq(i).attr('d') as string;
+
+                        if(unchangedTrackColors.includes(elems.eq(i).attr('stroke') as string)){
+                            expect(dim.d).to.equal(dimsBuff[i].d)
+                            expect(dim.height).to.equal(dimsBuff[i].height);
+                            expect(dim.width).to.equal(dimsBuff[i].width);
+                        }else{
+                            expect(dim.d).to.not.equal(dimsBuff[i].d)
+                            expect(dim.height).to.not.equal(dimsBuff[i].height);
+                            expect(dim.width).to.not.equal(dimsBuff[i].width);
+                        }
+                        dimsBuff[i] = dim;
+                    }
+                });
+
+                cy.get('.leaflet-overlay-pane > svg > g > path:nth-of-type(2)').then(elems => {
+                    for(let i = 1; i < elems.length; i++){
+                        expect(elems[i]).to.have.css('animation-duration', '44.75s');
+                    }
+                });
+
+                //pan back
+                cy.pan('.giraffe-plot',{x:dims.width/2,y:dims.height/2},{x:2,y:dims.height/2})
+                    .wait(500)
+
+                //Check all 12 endpoints are visible
+                cy.get('.leaflet-container').then(container => {
+                    cy.get('svg:first-of-type > g > path').then(elems => {
+
+                        cy.calcVisibleElements(container,elems).then(count => {
+                            expect(count).to.equal(12)
+                        })
+
+                        cy.calcHiddenElements(container,elems).then(count => {
+                            expect(count).to.equal(0)
+                        })
+                    })
+                })
+
+                cy.get('svg > g > path:first-of-type').then(elems => {
+
+                    for(let i = 1; i < elems.length; i++){
+                        let dim = {height: 0, width: 0, d: ''}
+                        dim.height = elems.eq(i).height() as number;
+                        dim.width = elems.eq(i).width() as number;
+                        dim.d = elems.eq(i).attr('d') as string;
+
+                        if(unchangedTrackColors.includes(elems.eq(i).attr('stroke') as string)){
+                            expect(dim.d).to.equal(dimsBuff[i].d)
+                            expect(dim.height).to.equal(dimsBuff[i].height);
+                            expect(dim.width).to.equal(dimsBuff[i].width);
+                        }else{
+                            expect(dim.d).to.not.equal(dimsBuff[i].d)
+                            expect(dim.height).to.not.equal(dimsBuff[i].height);
+                            expect(dim.width).to.not.equal(dimsBuff[i].width);
+                        }
+                    }
+                });
+
+                cy.get('.leaflet-overlay-pane > svg > g > path:nth-of-type(2)').then(elems => {
+                    for(let i = 1; i < elems.length; i++){
+                        expect(elems[i]).to.have.css('animation-duration', '44.75s');
+                    }
+                });
+
+            })
+
+    })
+
+    it('pans vertically', () => {
+
+        let dimsBuff: dimSVG[] = [];
+        let unchangedTrackColors = ['green', 'red', 'black', 'deeppink']
+
+        //Get base dims
+        cy.get('svg > g > path:first-of-type').then(elems => {
+
+            for(let i = 0; i < elems.length; i++){
+                let dim = {height: 0, width: 0, d: ''}
+                dim.height = elems.eq(i).height() as number;
+                dim.width = elems.eq(i).width() as number;
+                dim.d = elems.eq(i).attr('d') as string;
+                dimsBuff.push(dim);
+            }
+        })
+
+
+        let dims: {height: number,
+            width: number,
+            left: number,
+            top: number} = {height: 0, width: 0, left: 0, top: 0}
+        cy.get('div.giraffe-plot').then(container => {
+            cy.log('DEBUG typeof container.height() ' + typeof(container.height()));
+            dims.height = container.height() as number;
+            dims.width = container.width() as number;
+            let offset: JQuery.Coordinates | undefined = container.offset();
+            dims.top = offset === undefined ? 0 : offset.top;
+            dims.left = offset === undefined ? 0 : offset.left;
+        }).wait(1000)
+            .then(() => {
+                // pan away
+                cy.pan('.giraffe-plot',{x:dims.width/2,y:dims.height/2},{x:dims.width/2,y:dims.height - 2})
+                    .wait(500)
+
+                //Check only 5 endpoints are visible
+                cy.get('.leaflet-container').then(container => {
+                    cy.get('svg:first-of-type > g > path').then(elems => {
+
+                        cy.calcVisibleElements(container,elems).then(count => {
+                            expect(count).to.equal(9)
+                        })
+
+                        cy.calcHiddenElements(container,elems).then(count => {
+                            expect(count).to.equal(3)
+                        })
+                    })
+                })
+
+                //Check dim vals changed for some tracks
+                cy.get('svg > g > path:first-of-type').then(elems => {
+
+                    for(let i = 1; i < elems.length; i++){
+                        let dim = {height: 0, width: 0, d: ''}
+                        dim.height = elems.eq(i).height() as number;
+                        dim.width = elems.eq(i).width() as number;
+                        dim.d = elems.eq(i).attr('d') as string;
+
+                        if(unchangedTrackColors.includes(elems.eq(i).attr('stroke') as string)){
+                            expect(dim.d).to.equal(dimsBuff[i].d)
+                            expect(dim.height).to.equal(dimsBuff[i].height);
+                            expect(dim.width).to.equal(dimsBuff[i].width);
+                        }else{
+                            expect(dim.d).to.not.equal(dimsBuff[i].d)
+                            expect(dim.height).to.not.equal(dimsBuff[i].height);
+                            expect(dim.width).to.not.equal(dimsBuff[i].width);
+                        }
+                        dimsBuff[i] = dim;
+                    }
+                });
+
+                //animation should not be affected
+                cy.get('.leaflet-overlay-pane > svg > g > path:nth-of-type(2)').then(elems => {
+                    for(let i = 1; i < elems.length; i++){
+                        expect(elems[i]).to.have.css('animation-duration', '44.75s');
+                    }
+                });
+
+                //pan back
+                cy.pan('.giraffe-plot',{x:dims.width/2,y:dims.height/2},{x:dims.width/2,y:2})
+                    .wait(500)
+
+                //Check all 12 endpoints are visible
+                cy.get('.leaflet-container').then(container => {
+                    cy.get('svg:first-of-type > g > path').then(elems => {
+
+                        cy.calcVisibleElements(container,elems).then(count => {
+                            expect(count).to.equal(12)
+                        })
+
+                        cy.calcHiddenElements(container,elems).then(count => {
+                            expect(count).to.equal(0)
+                        })
+                    })
+                })
+
+                cy.get('svg > g > path:first-of-type').then(elems => {
+
+                    for(let i = 1; i < elems.length; i++){
+                        let dim = {height: 0, width: 0, d: ''}
+                        dim.height = elems.eq(i).height() as number;
+                        dim.width = elems.eq(i).width() as number;
+                        dim.d = elems.eq(i).attr('d') as string;
+
+                        if(unchangedTrackColors.includes(elems.eq(i).attr('stroke') as string)){
+                            expect(dim.d).to.equal(dimsBuff[i].d)
+                            expect(dim.height).to.equal(dimsBuff[i].height);
+                            expect(dim.width).to.equal(dimsBuff[i].width);
+                        }else{
+                            expect(dim.d).to.not.equal(dimsBuff[i].d)
+                            expect(dim.height).to.not.equal(dimsBuff[i].height);
+                            expect(dim.width).to.not.equal(dimsBuff[i].width);
+                        }
+                    }
+                });
+
+                //animation should not be affected
+                cy.get('.leaflet-overlay-pane > svg > g > path:nth-of-type(2)').then(elems => {
+                    for(let i = 1; i < elems.length; i++){
+                        expect(elems[i]).to.have.css('animation-duration', '44.75s');
+                    }
+                });
+
+            })
+    })
+
+
 
 })
