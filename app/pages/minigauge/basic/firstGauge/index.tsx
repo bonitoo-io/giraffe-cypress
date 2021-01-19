bbe11a253dac7a015158394a58496117178d818e
Cubone21 <kyuboun@gmail.com>
2021-01-19 12:42:49 +0100

    feat: add minigauge component

---
+++ app/pages/minigauge/basic/firstGauge/index.tsx
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+
+import dynamic from "next/dynamic";
+
+const GaugeMini = dynamic(() => {return import('../../../../components/GaugeMini')},{ssr: false});
+
+let data = [];
+
+export async function getServerSideProps(){
+
+    const res = await fetch(`http://localhost:3000/api/influx/query`)
+
+    console.log("DEBUG GeoTest res XXX " + res);
+
+    //const data = {}
+    const data = await res.json();
+
+    console.log("DEBUG GeoTest getServerSideProps Data XXX: " + JSON.stringify(data));
+    return {props: {data} }
+}
+
+export default function Home({ data }){
+    return(
+        <div>
+            <Head>
+                <title>Hello Minigauge</title>
+            </Head>
+                <section style={{height:"608px", width: "608px" }}>
+                    <p>Minigauge, minigauge</p>
+                    <div style={{height: "600px", width: "600px", position: "absolute", top: 0, left: 0}}>
+                        <GaugeMini data={data}/>
+                    </div>
+
+                </section>
+        </div>
+    );
+}
