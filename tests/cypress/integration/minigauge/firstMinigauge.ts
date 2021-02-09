588c0e9816be1b050acd2fb8fd72cf6d68112f87
karel-rehor <karel.k.rehor@email.cz>
2021-02-09 10:54:19 +0100

    feat: add primitive visual comparison to minigauge test

--- tests/cypress/integration/minigauge/firstMinigauge.ts
+++ tests/cypress/integration/minigauge/firstMinigauge.ts
@@ -2,6 +2,9 @@ before(() => {
     cy.resetDB();
 })
 
+
+const imageExpectedDir = "./cypress/fixtures/images/minigauge"
+
 describe('First Minigauge', () => {
 
     before('Load Data', () => {
@@ -17,9 +20,53 @@ describe('First Minigauge', () => {
         cy.screenshot();
     })
 
-
     it('loads minigauge graph', () => {
         cy.get('.giraffe-plot').should('be.visible')
-        cy.get('.gauge-mini-bar').should('have.length', 3)
+        cy.get('#subcontainer .giraffe-layers').should('have.length', 3)
+        cy.get('#subcontainer:nth-of-type(1) .giraffe-layers').should('exist')
+        cy.get('#subcontainer:nth-of-type(1) .giraffe-layers svg').screenshot('miniGaugeCZ')
+        cy.get('#subcontainer:nth-of-type(2) .giraffe-layers svg').screenshot('miniGaugeSK')
+        cy.get('#subcontainer:nth-of-type(3) .giraffe-layers svg').screenshot('miniGaugeDE')
+
+        cy.task('log', 'HELLO')
+        cy.task('fsCheck', './cypress/fixtures/images/minigauge')
+        cy.task('compareImageFiles', {
+            file1: imageExpectedDir + '/miniGaugeCZ.png',
+            file2: './cypress/screenshots/minigauge/firstMinigauge.ts/miniGaugeCZ.png'})
+            .then((result) => {
+                console.log(`DEBUG compare Result ${JSON.stringify(result)}`)
+                if(result){
+                    assert.equal(result.pct, 0, 'Match PNG files by percentage')
+                    assert.equal(result.pixelDif, 0, 'Natch PNG file by pixels')
+                }else{
+                    throw 'compareImageFiles result undefined'
+                }
+            })
+
+        cy.task('compareImageFiles', {
+            file1: imageExpectedDir + '/miniGaugeSK.png',
+            file2: './cypress/screenshots/minigauge/firstMinigauge.ts/miniGaugeSK.png'})
+            .then((result) => {
+                console.log(`DEBUG compare Result ${JSON.stringify(result)}`)
+                if(result){
+                    assert.equal(result.pct, 0, 'Match PNG files by percentage')
+                    assert.equal(result.pixelDif, 0, 'Match PNG file by pixels')
+                }else{
+                    throw 'compareImageFiles result undefined'
+                }
+            })
+        cy.task('compareImageFiles', {
+            file1: imageExpectedDir + '/miniGaugeDE.png',
+            file2: './cypress/screenshots/minigauge/firstMinigauge.ts/miniGaugeDE.png'})
+            .then((result) => {
+                console.log(`DEBUG compare Result ${JSON.stringify(result)}`)
+                if(result){
+                    assert.equal(result.pct, 0, 'Match PNG files by percentage')
+                    assert.equal(result.pixelDif, 0, 'Match PNG file by pixels')
+                }else{
+                    throw 'compareImageFiles result undefined'
+                }
+            })
     })
+
 })
--- tests/cypress/plugins/index.ts
+++ tests/cypress/plugins/index.ts
@@ -1,8 +1,16 @@
-import path from 'path'
+import * as path from 'path'
+import * as fs from 'fs'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import {PNG} from 'pngjs'
+
+import * as pixelmatch from 'pixelmatch'
+
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on: any, config: any) => {
+
     // `on` is used to hook into various events Cypress emits
     // `config` is the resolved Cypress config
     on('task', {
@@ -15,8 +23,75 @@ module.exports = (on: any, config: any) => {
         },
         getEnvVal(val: string){
             return JSON.stringify(process.env[val]);
+        },
+        fsCheck(dir = '.'){
+            const listing = fs.readdirSync(dir)
+            console.log('fsCheck listing ' + listing)
+            return listing
+        },
+        async compareImageFiles(args: {file1: string, file2: string}){
+            let matchedPixels = 0;
+            let pct = 0;
+
+
+            const expectedImg : PNG = await parseImage(args.file1);
+            const actualImg : PNG = await parseImage(args.file2);
+            console.log(`DEBUG expectedImg ${expectedImg}`)
+            console.log(`DEBUG actualImg ${actualImg}`)
+            const diff : PNG = new PNG({
+                width: Math.max(actualImg.width, expectedImg.width),
+                height: Math.max(actualImg.height, expectedImg.height),
+            });
+            console.log(`DEBUG diff ${diff} width ${diff.width} height ${diff.height}`)
+            console.log(`DEBUG actualImage ${actualImg.width} x ${actualImg.height}`)
+            console.log(`DEBUG width  ${expectedImg.width - actualImg.width}`)
+            console.log(`DEBUG height  ${expectedImg.height - actualImg.height}`)
+            const actualImgAdjusted = adjustCanvas(actualImg, diff.width, diff.height)
+            const expectedImgAdjusted = adjustCanvas(expectedImg, diff.width, diff.height)
+
+            matchedPixels = pixelmatch(expectedImgAdjusted.data, actualImgAdjusted.data, diff.data, diff.width, diff.height, {threshold: 0.1})
+            pct = (matchedPixels / diff.width / diff.height) ** 0.5
+            console.log(`DEBUG match ${matchedPixels}`)
+            return {pixelDif: matchedPixels, pct: pct};
         }
     })
+}
 
+const parseImage = async (imageFile: string) => {
+    return new Promise((resolve, reject) => {
+        if (!fs.existsSync(imageFile)) {
+            reject(new Error(`Snapshot ${imageFile} does not exist.`));
+            return;
+        }
 
+        const fd = fs.createReadStream(imageFile);
+        /* eslint-disable func-names */
+        fd.pipe(new PNG())
+            .on('parsed', function() {
+                //console.log(`DEBUG parsed image ${JSON.stringify(this)}`)
+                const that = this;
+                resolve(that);
+            })
+            .on('error', (error: any) => reject(error));
+        /* eslint-enable func-names */
+    });
+}
+
+const adjustCanvas = (image: PNG, width: number, height: number) => {
+    if(image.width === width && image.heigth === height){
+        return image;
+    }
+
+    const adjustedCanvas : PNG = new PNG({
+        height: height,
+        width: width,
+        bitDepth: image.bitDepth,
+        inputHasAlpha: true
+    })
+
+    console.log(`DEBUG adjustedCanvas ${adjustedCanvas.width} x ${adjustedCanvas.height}`)
+
+    PNG.bitblt(image, adjustedCanvas, 0, 0, image.width, image.height, 0, 0)
+
+    return adjustedCanvas;
 }
