c5221ddcf5b1ba4acf8b484a2b558ff318c8014d
karel-rehor <karel.k.rehor@email.cz>
2021-03-25 15:17:29 +0100

    fix: add png compare for s2Circle and center-zoom tests

--- tests/cypress/integration/geowidget/s2/s2Circles.ts
+++ tests/cypress/integration/geowidget/s2/s2Circles.ts
@@ -4,7 +4,18 @@ before(() => {
 
 describe('GeoWidget - S2 - Map with Circles', () => {
 
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const ied = Cypress.config("imageExpectedDir");
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const iad = Cypress.config("imageActualDir");
+    const defaultWait = 1000;
+    const screenshotCompareDir = `./cypress/screenshots/geowidget/s2/s2Circles.ts/${iad}`
+
+
     before('Load Data', () => {
+        cy.task('cleanDir', screenshotCompareDir);
         cy.datagenFromLPFixture('influx/futuroscope01.lp')
         cy.wait(1000) //wait for backend to process data
     })
@@ -18,9 +29,14 @@ describe('GeoWidget - S2 - Map with Circles', () => {
     })
 
     it('loads the map', () => {
-        cy.log('map loaded')
+        //cy.wait(defaultWait * 3) //wait for .leaflet-animated fade ins to complete and first load
+        cy.waitForLeaflet()
+        cy.wait(1000) // wait an extra second - can be slow
         cy.get('[data-testid=geowidget-hash-cirlces]').should('be.visible');
-        //TODO add png compare test once #440 is fixed
+        cy.get('div.geo > div').screenshot(iad +'/s2CircleLoad00');
+        cy.comparePNGFiles(`${ied}/s2/s2Circles/s2CircleLoad00.png`,
+            `${screenshotCompareDir}/s2CircleLoad00.png`)
+
     })
 
     it('can retrieve S2 style datapoints', () => {
@@ -30,19 +46,23 @@ describe('GeoWidget - S2 - Map with Circles', () => {
         cy.get('[data-testid=s2_1]').scrollIntoView().should('have.text','47f934d2a3cb');
         cy.get('[data-testid=s2_3]').scrollIntoView().should('have.text', '4807303260c9');
         cy.get('[data-testid=s2_5]').scrollIntoView().should('have.text','47fda9e43c2b');
-        cy.get('[data-testid=select-ints]').select('4');
+        cy.get('[data-testid=select-ints]').select('5');
 
-        //TODO better wait
-        cy.wait(500)
+        cy.waitForLeaflet()
+        cy.get('div.geo > div').screenshot(iad +'/s2Circle01');
+        cy.comparePNGFiles(`${ied}/s2/s2Circles/s2Circle01.png`,
+            `${screenshotCompareDir}/s2Circle01.png`)
 
-        cy.get('[data-testid=s2_1]').should('have.text', '47f');
-        cy.get('[data-testid=s2_3]').should('have.text', '481');
-        cy.get('[data-testid=s2_5]').should('have.text', '47f');
+        cy.get('[data-testid=s2_1]').should('have.text', '47fc');
+        cy.get('[data-testid=s2_3]').should('have.text', '4804');
+        cy.get('[data-testid=s2_5]').should('have.text', '47fc');
 
         cy.get('[data-testid=select-ints]').select('10');
 
-        //TODO better wait
-        cy.wait(500)
+        cy.waitForLeaflet()
+        cy.get('div.geo > div').screenshot(iad +'/s2Circle02');
+        cy.comparePNGFiles(`${ied}/s2/s2Circles/s2Circle02.png`,
+            `${screenshotCompareDir}/s2Circle02.png`)
 
         cy.get('[data-testid=s2_1]').should('have.text', '47f935');
         cy.get('[data-testid=s2_3]').should('have.text', '480731');
@@ -50,8 +70,10 @@ describe('GeoWidget - S2 - Map with Circles', () => {
 
         cy.get('[data-testid=select-ints]').select('22');
 
-        //TODO better wait
-        cy.wait(500)
+        cy.waitForLeaflet()
+        cy.get('div.geo > div').screenshot(iad +'/s2Circle03');
+        cy.comparePNGFiles(`${ied}/s2/s2Circles/s2Circle03.png`,
+            `${screenshotCompareDir}/s2Circle03.png`)
 
         cy.get('[data-testid=s2_1]').scrollIntoView().should('have.text','47f934d2a3cb');
         cy.get('[data-testid=s2_3]').scrollIntoView().should('have.text', '4807303260c9');
--- tests/cypress/integration/geowidget/simpleMapMarkerCenter.ts
+++ tests/cypress/integration/geowidget/simpleMapMarkerCenter.ts
@@ -4,12 +4,23 @@ before(() => {
 
 describe('GeoWidget - Center Map on Markers', () => {
 
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const ied = Cypress.config("imageExpectedDir");
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const iad = Cypress.config("imageActualDir");
+    const screenshotCompareDir = `./cypress/screenshots/geowidget/simpleMapMarkerCenter.ts/${iad}`
+
+
     before('Load Data', () => {
+        cy.task('cleanDir', screenshotCompareDir);
         cy.datagenFromLPFixture('influx/futuroscope01.lp')
         cy.wait(1000) //wait for backend to process data
     })
 
     beforeEach('load Page', () => {
+
         cy.visit('/geowidget/basic/simpleMapMarkersCenter')
     })
 
@@ -20,43 +31,88 @@ describe('GeoWidget - Center Map on Markers', () => {
     it('loads the map', () => {
         cy.log('map loaded')
         cy.get('[data-testid=geowidget-markers]').should('be.visible');
-        //TODO add png compare test once #440 is fixed
+        cy.waitForLeaflet()
+        cy.get('div.geo > div').screenshot(iad +'/CenterMapMarkersFirstLoad');
+        cy.comparePNGFiles(`${ied}/simpleMapMarkerCenter/CenterMapMarkersFirstLoad.png`,
+            `${screenshotCompareDir}/CenterMapMarkersFirstLoad.png`)
     })
 
     it('dynamically centers map on latlon datapoints', () => {
-        //TODO check baseline PNG comparison
         cy.get('[data-testid=lat_1]').click()
-        //TODO png comparison of change after #440 fix
+        cy.waitForLeaflet()
+        cy.wait(1000) //above can be slow 1 time in 10
+        cy.get('div.geo > div').screenshot(iad +'/DynamicCenter01');
+        cy.comparePNGFiles(`${ied}/simpleMapMarkerCenter/DynamicCenter01.png`,
+            `${screenshotCompareDir}/DynamicCenter01.png`)
+
         cy.get('[data-testid=lon_3]').click()
-        //TODO png comparison of change
+        cy.waitForLeaflet()
+
+        cy.get('div.geo > div').screenshot(iad +'/DynamicCenter03');
+        cy.comparePNGFiles(`${ied}/simpleMapMarkerCenter/DynamicCenter03.png`,
+            `${screenshotCompareDir}/DynamicCenter03.png`)
+
         cy.get('[data-testid=lat_5]').click()
-        //TODO png comparison of change
+        cy.waitForLeaflet()
+
+        cy.get('div.geo > div').screenshot(iad +'/DynamicCenter05');
+        cy.comparePNGFiles(`${ied}/simpleMapMarkerCenter/DynamicCenter05.png`,
+            `${screenshotCompareDir}/DynamicCenter05.png`)
+
         cy.get('[data-testid=btn-recenter]').click()
-        //TODO png comparison of change
+        cy.waitForLeaflet()
+
+        cy.get('div.geo > div').screenshot(iad +'/DynamicCenterRecenter');
+        cy.comparePNGFiles(`${ied}/simpleMapMarkerCenter/DynamicCenterRecenter.png`,
+            `${screenshotCompareDir}/DynamicCenterRecenter.png`)
+
     })
 
     it('dynamically zooms on centered latlon datapoints', () => {
-        //TODO check baseline PNG comparison
+        cy.waitForLeaflet()
+        cy.get('div.geo > div').screenshot(iad +'/DynamicZoom00');
+        cy.comparePNGFiles(`${ied}/simpleMapMarkerCenter/DynamicZoom00.png`,
+            `${screenshotCompareDir}/DynamicZoom00.png`)
+
         cy.get('[data-testid=lat_1]').click()
+        cy.waitForLeaflet()
+
         cy.get('[data-testid=select-ints]').select('12')
-        cy.wait(1000) //give openstreet map time to load
-        //TODO png comparison of change after #440 fix
+        cy.waitForLeaflet()
+        cy.wait(1000) //this zoom seems slower than others
+
+        cy.get('div.geo > div').screenshot(iad +'/DynamicZoom01');
+        cy.comparePNGFiles(`${ied}/simpleMapMarkerCenter/DynamicZoom01.png`,
+            `${screenshotCompareDir}/DynamicZoom01.png`)
+
         cy.get('[data-testid=lon_3]').click() //should still be level 12
-        //TODO png comparison of change after #440 fix
+        cy.waitForLeaflet()
+
         cy.get('[data-testid=select-ints]').select('16')
-        cy.wait(1000) //give openstreet map time to load
-        //TODO png comparison of change after #440 fix
+        cy.waitForLeaflet()
+        cy.wait(1000) //this zoom seems slower than others
+        cy.get('div.geo > div').screenshot(iad +'/DynamicZoom02');
+        cy.comparePNGFiles(`${ied}/simpleMapMarkerCenter/DynamicZoom02.png`,
+            `${screenshotCompareDir}/DynamicZoom02.png`)
+
         cy.get('[data-testid=lat_5]').click()
-        //TODO png comparison of change after #440 fix
+        cy.waitForLeaflet()
+
         cy.get('[data-testid=select-ints]').select('4') // now zoom out
-        cy.wait(1000) //give openstreet map time to load
-        //TODO png comparison of change
+        cy.waitForLeaflet()
+
+        cy.get('div.geo > div').screenshot(iad +'/DynamicZoom03');
+        cy.comparePNGFiles(`${ied}/simpleMapMarkerCenter/DynamicZoom03.png`,
+            `${screenshotCompareDir}/DynamicZoom03.png`)
+
         cy.get('[data-testid=btn-recenter]').click()
-        //TODO png comparison of change
+        cy.waitForLeaflet()
+
         cy.get('[data-testid=select-ints]').select('8') // return to baseline
-        cy.wait(1000) //give openstreet map time to load
-        //TODO png comparison of change
+        cy.waitForLeaflet()
+        cy.wait(1000)
+        cy.get('div.geo > div').screenshot(iad +'/DynamicZoom04');
+        cy.comparePNGFiles(`${ied}/simpleMapMarkerCenter/DynamicZoom04.png`,
+            `${screenshotCompareDir}/DynamicZoom04.png`)
     })
-
-
 })
